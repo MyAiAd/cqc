9e09c24b6e23dc6d5b9d66e8647721ba13e8f2d7
Sage <Sage@MyAi.ad>
2025-07-09 19:44:28 +0100

    Clean up: remove debug function from main.tsx as issue is resolved

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Add debug functions to window object for testing
-import { debugPractices } from './services/dataService';
-
-declare global {
-  interface Window {
-    debugPractices: () => Promise<any>;
-  }
-}
-
-// Make debug functions available globally
-window.debugPractices = debugPractices;
-
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <App />
